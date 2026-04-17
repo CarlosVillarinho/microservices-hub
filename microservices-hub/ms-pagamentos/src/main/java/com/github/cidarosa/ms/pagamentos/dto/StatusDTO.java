9c667b3913a2b5ca05c9d078f6e3bbd0e09b6381
Carlos Villarinho <worksvillarinho@gmail.com>
2026-04-17 11:43:57 -0300

    Delete microservices-hub directory

--- microservices-hub/ms-pagamentos/src/main/java/com/github/cidarosa/ms/pagamentos/dto/StatusDTO.java
+++
@@ -1,8 +0,0 @@
-package com.github.cidarosa.ms.pagamentos.dto;
-
-import com.github.cidarosa.ms.pagamentos.entities.Status;
-
-public class StatusDTO {
-    //ATRIBUTOS
-    private Status status;
-}
--- microservices-hub/ms-pagamentos/src/main/java/com/github/cidarosa/ms/pagamentos/repository/PagamentoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.cidarosa.ms.pagamentos.repository;
-
-import com.github.cidarosa.ms.pagamentos.entities.Pagamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
-}
